eb37e0a3161eff606a1a4f73bb609e68f2f40a61
mo7amedsakr <msakr456@gmail.com>
2020-10-07 16:53:15 +0200

    debug

--- client/src/containers/ShortenItSection/ShortenItSection.js
+++ client/src/containers/ShortenItSection/ShortenItSection.js
@@ -38,6 +38,9 @@ const ShortenItSection = () => {
         }
       );
       setLoading(false);
+      console.log(res);
+      console.log(res.data);
+      console.log(res.data.url);
       setShortenLinks((prev) => [
         {
           original: inputValue,
